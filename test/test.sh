d93f06a7be9b664ca54f731c9c494dba81ba2130
Eric DELANGHE <edelangh@e1r11p4.42.fr>
2016-03-04 18:48:57 +0100

    add unitest

---
+++ test/test.sh
@@ -0,0 +1,40 @@
+#/bin/bash
+
+i=3;
+nbTest=10;
+args='-s';
+genPath='../puzzle/npuzzle-gen.py';
+exePath='../npuzzle';
+echo "=== 1x1 Test ==="
+$exePath "./1-S-0.txt" >& /dev/null;
+if [ $? = "0" ]; then echo ok ; else echo Bad ; fi
+echo "=== 2x2 Solv ==="
+$exePath "./2-S-0.txt" >& /dev/null;
+if [ $? = "0" ]; then echo ok ; else echo Bad ; fi
+echo "=== 2x2 UnSolv ==="
+$exePath "./2-U-0.txt" >& /dev/null;
+if [ $? = "1" ]; then echo ok ; else echo Bad ; fi
+
+echo "=== Solvable ==="
+while [ $i -lt $nbTest ] ; do
+	echo "Test: puzzle "$i"x"$i;
+	file='/tmp/puzzle-'$i'.'$$'.txt';
+	$genPath $args $i > file;
+	$exePath file >& /dev/null;
+	if [ $? = "0" ]; then echo ok ; else echo Bad ; fi
+	rm file;
+	let i+=1;
+done;
+
+i=3;
+args='-u';
+echo "=== UnSolvable ==="
+while [ $i -lt $nbTest ] ; do
+	echo "Test: puzzle "$i"x"$i;
+	file='/tmp/puzzle-'$i'.'$$'.txt';
+	$genPath $args $i > file;
+	$exePath file >& /dev/null;
+	if [ $? = "1" ]; then echo ok ; else echo Bad ; fi
+	rm file;
+	let i+=1;
+done;
