0921b74563161e9e1fbabb75a1685251c5219ad4
Eric DELANGHE <edelangh@e1r11p4.42.fr>
2016-03-07 12:54:25 +0100

    norm solver

--- Solver.cpp
+++ Solver.cpp
@@ -6,56 +6,56 @@
 
 Solver::Solver(State* root)
 {
-    opened.insert(root);
-    candidates.insert(root);
+	opened.insert(root);
+	candidates.insert(root);
 };
+
 Solver::Result Solver::Step()
 {
-    Result result = Result(0, 0);
-    if (!opened.empty())
-    {
-        State* e = *candidates.begin();
-        if (e->is_final())
-        {
-            result.finished = true;
-            result.movements = e->get_movements();
-            return (result);
-        }
-        else
-        {
-            opened.erase(e);
-            candidates.erase(e);
-            closed.insert(e);
-            //FOREACH POSSIBILITY S FROM E
-            {
-                State* s;
+	Result result = Result(0, 0);
 
-                auto openedFound = opened.find(s);
-                auto closedFound = closed.find(s);
+	if (!opened.empty())
+	{
+		State* e = *candidates.begin();
+		if (e->is_final())
+		{
+			result.finished = true;
+			result.movements = e->get_movements();
+			return (result);
+		}
+		else
+		{
+			opened.erase(e);
+			candidates.erase(e);
+			closed.insert(e);
+			//FOREACH POSSIBILITY S FROM E
+			{
+				State* s;
 
-                if (openedFound == opened.end() && closedFound == closed.end())
-                {
-                    opened.insert(s);
-                }
-                else
-                {
-                    if (s->get_distance() < ()->get_distance())
-                    {
-                        opened->
-                    }
-                }
+				auto openedFound = opened.find(s);
+				auto closedFound = closed.find(s);
 
-            }
-
-        }
-    }
+				if (openedFound == opened.end() && closedFound == closed.end())
+				{
+					opened.insert(s);
+				}
+				else
+				{
+					if (s->get_distance() < ()->get_distance())
+					{
+						opened->
+					}
+				}
+			}
+		}
+	}
 }
+
 Solver::~Solver();
 
-
 Solver::Result::Result(int timeComplexity, int sizeComplexity):
-timeComplexity(timeComplexity),
-sizeComplexity(sizeComplexity),
-movements(nullptr),
-finished(false)
+	timeComplexity(timeComplexity),
+	sizeComplexity(sizeComplexity),
+	movements(nullptr),
+	finished(false)
 {};
--- Solver.hpp
+++ Solver.hpp
@@ -12,28 +12,25 @@
 #include "State.hpp"
 
 class Solver {
+	public:
+		class Result
+		{
+			public:
+				int timeComplexity;
+				int sizeComplexity;
+				std::list<State::Movement>* movements;
+				bool finished;
+				Result(int timeComplexity, int sizeComplexity);
+		};
 
-public:
-    class Result
-    {
-    public:
-        int timeComplexity;
-        int sizeComplexity;
-        std::list<State::Movement>* movements;
-        bool finished;
-        Result(int timeComplexity, int sizeComplexity);
-    };
+		Solver(State* root);
+		Result Step();
+		~Solver();
 
-    Solver(State* root);
-    Result Step();
-    ~Solver();
-
-private:
-    std::unordered_set<State*, State::hash_unordered_set, State::pred_unordered_set> opened;
-    std::unordered_set<State*, State::hash_unordered_set, State::pred_unordered_set> closed;
-    std::set<State*, State::comp_set> candidates;
-
+	private:
+		std::unordered_set<State*, State::hash_unordered_set, State::pred_unordered_set> opened;
+		std::unordered_set<State*, State::hash_unordered_set, State::pred_unordered_set> closed;
+		std::set<State*, State::comp_set> candidates;
 };
-
 
 #endif //NPUZZLE_SOLVER_H
